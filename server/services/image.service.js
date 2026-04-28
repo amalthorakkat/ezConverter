db4e4f6ed31bfe8cd16c17269150d848890c3e7c
amalthorakkat <amalthorakkat@gmail.com>
2026-04-28 20:16:04 +0530

    Fixed send and download!

--- server/services/image.service.js
+++ server/services/image.service.js
@@ -1,11 +1,17 @@
 const sharp = require("sharp");
 const path = require("path");
+const fs = require("fs");
 
 module.exports = async (inputPath, format) => {
   const outputFileName =
-    Date.now() + "-" + Math.round(Math.random() * 1e9) + "-" + format;
+    Date.now() + "-" + Math.round(Math.random() * 1e9) + "." + format;
 
-  const outputPath = path.join(__dirname, "../outputs", outputFileName);
+  const outputDir = path.join(__dirname, "../outputs");
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir, { recursive: true });
+  }
+
+  const outputPath = path.join(outputDir, outputFileName);
 
   let pipeline = sharp(inputPath);
 
